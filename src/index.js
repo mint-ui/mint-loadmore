56515582174663177ece01e547c85160ebc474f3
Leopoldthecoder <Leopoldthecuber@gmail.com>
2016-05-28 19:51:25 +0800

    replace kb-spinner with mint-spinner

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import Loadmore from './loadmore.vue';
-import FadingCircle from 'kb-spinner/lib/fading-circle';
-import 'kb-spinner/lib/fading-circle/style.css';
+import { FadingCircle } from 'mint-spinner';
 Vue.component('spinner', FadingCircle);
 export default Loadmore;
